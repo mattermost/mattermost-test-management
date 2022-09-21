36b1375fc29aed836ca3b75598b5703cbcadb122
Pantelis Vratsalis <pvratsalis@gmail.com>
2022-09-21 13:51:35 -0400

    (feat/www): new route that redirect to a test case by ID or key

--- www/fresh.gen.ts
+++ www/fresh.gen.ts
@@ -7,7 +7,8 @@ import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_500.tsx";
 import * as $2 from "./routes/gfm.css.ts";
 import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/test-cases/[...slug].tsx";
+import * as $4 from "./routes/test-case/[testCaseId].tsx";
+import * as $5 from "./routes/test-cases/[...slug].tsx";
 
 const manifest = {
   routes: {
@@ -15,7 +16,8 @@ const manifest = {
     "./routes/_500.tsx": $1,
     "./routes/gfm.css.ts": $2,
     "./routes/index.tsx": $3,
-    "./routes/test-cases/[...slug].tsx": $4,
+    "./routes/test-case/[testCaseId].tsx": $4,
+    "./routes/test-cases/[...slug].tsx": $5,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ www/routes/test-case/[testCaseId].tsx
@@ -0,0 +1,24 @@
+import tcKeyAndPath from "../../../data/key-and-path.json" assert { type: "json" };
+
+type KeyAndPath = { key: string; path: string, id: number }[];
+const keyAndPath = tcKeyAndPath as KeyAndPath;
+
+export const handler = {
+    GET(_req, ctx) {
+      const testCaseId = ctx.params.testCaseId.toUpperCase();
+      let foundEntry = keyAndPath.filter(it => it.key.toUpperCase() === testCaseId);
+
+      if (foundEntry.length === 0) {
+        foundEntry = keyAndPath.filter(it => it.id.toString() === testCaseId);
+      }
+
+      if (foundEntry.length !== 0) {
+        return new Response("", {
+          status: 301,
+          headers: { location: `/test-cases/${foundEntry[0].path}/${foundEntry[0].key.toLowerCase()}` },
+        });
+      } else {
+        return ctx.renderNotFound();
+      }
+  }
+};
