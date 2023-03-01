a62e5b39b2c1be0fde3456e2223f0210592085e0
Saturnino Abril <saturnino.abril@gmail.com>
2023-03-01 10:14:06 +0800

    chore: update dependencies (#75)

--- www/utils/deps.ts
+++ www/utils/deps.ts
@@ -1,1 +1,3 @@
+export * as gfm from "https://deno.land/x/gfm@0.2.1/mod.ts";
+export { extract as extractFrontMatter } from "$std/encoding/front_matter/yaml.ts";
 export { sortBy } from "$std/collections/sort_by.ts";
